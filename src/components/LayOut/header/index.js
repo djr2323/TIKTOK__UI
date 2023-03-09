c1b72bad17abc7ef83249799af99d9160162f50c
djr2323 <101247795+djr2323@users.noreply.github.com>
2023-03-09 17:56:40 +0700

    dựng ui phần header - search

--- src/components/LayOut/header/index.js
+++ src/components/LayOut/header/index.js
@@ -1,5 +1,8 @@
 import Styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(Styles);
 
@@ -8,7 +11,23 @@ function Header() {
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 {/* logo */}
-                {/* seacrh */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="" />
+                </div>
+                {/* search */}
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck="false" />
+                    <button className={cx('close-btn')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                {/* actions */}
+                <div className={cx('actions')}></div>
             </div>
         </header>
     );
