84b78e04f064603ecfd6b2ac7d38793620d069eb
djr2323 <101247795+djr2323@users.noreply.github.com>
2023-03-21 23:14:33 +0700

    dựng ui header (thêm sửa menu,tippy)

--- src/components/LayOut/header/index.js
+++ src/components/LayOut/header/index.js
@@ -4,13 +4,39 @@ import classNames from 'classnames/bind';
 import images from '~/assets/images';
 import Button from '~/components/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSignIn, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleQuestion,
+    faCircleXmark,
+    faEarthAsia,
+    faEllipsisVertical,
+    faKeyboard,
+    faMagnifyingGlass,
+    faSignIn,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItems';
+import Menu from '~/components/Popper/Menu';
 
 const cx = classNames.bind(Styles);
+
+const Menu_Items = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -64,6 +90,12 @@ function Header() {
                     <Button rounded primary rightIcon={<FontAwesomeIcon icon={faSignIn} />}>
                         Log In
                     </Button>
+
+                    <Menu items={Menu_Items}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
